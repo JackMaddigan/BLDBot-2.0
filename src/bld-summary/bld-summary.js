3701510188dc810678512da092c7df950f33ffc7
Jack Maddigan <jack.g.maddigan@gmail.com>
2026-04-01 10:04:02 +1300

    fix bld-summary when embed too long

--- src/bld-summary/bld-summary.js
+++ src/bld-summary/bld-summary.js
@@ -46,106 +46,95 @@ async function runSummary(client) {
   }
 
   try {
-    const summaryEmbed = makeSummaryEmbed(stats);
+    const embeds = [...makeSummaryEmbeds(stats), makeStatsEmbed(stats)];
     const recordsChannel = await client.channels.fetch(
       process.env.recordsChannelId
     );
-    await recordsChannel.send({ embeds: [summaryEmbed] });
+    await recordsChannel.send({ embeds });
   } catch (error) {
     console.error("Error making or sending summary embed!", error);
     return;
   }
-
-  try {
-    const statsEmbed = makeStatsEmbed(stats);
-    const generalChannel = await client.channels.fetch(
-      process.env.generalChannelId
-    );
-    await generalChannel.send({ embeds: [statsEmbed] });
-  } catch (error) {
-    console.error("Error making or sending stats embed!", error);
-    return;
-  }
 }
 
-async function processCubingChinaResults(
-  stats,
-  today,
-  oneWeekAgo,
-  resultsToBeat
-) {
-  const comps = await getCubingChinaComps(oneWeekAgo, today);
-  console.log("China comps:s", comps);
-  const roundIds = ["f", "1", "2", "3"];
-  // check every comp in the list
-  const browser = await puppeteer.launch();
-  for (const comp of comps) {
-    const compEvents = comp.events.filter((eventId) => eventIds.has(eventId));
-    const competitorList = await getCubingChinaCompetitorList(browser, comp.id);
-    for (const eventId of compEvents)
-      for (const roundId of roundIds) {
-        const link = `https://cubing.com/live/${comp.id
-          .split(/(?=[A-Z])|(?=\d{4})/)
-          .join("-")}#!/event/${eventId}/${roundId}/all`;
+// async function processCubingChinaResults(
+//   stats,
+//   today,
+//   oneWeekAgo,
+//   resultsToBeat
+// ) {
+//   const comps = await getCubingChinaComps(oneWeekAgo, today);
+//   console.log("China comps:s", comps);
+//   const roundIds = ["f", "1", "2", "3"];
+//   // check every comp in the list
+//   const browser = await puppeteer.launch();
+//   for (const comp of comps) {
+//     const compEvents = comp.events.filter((eventId) => eventIds.has(eventId));
+//     const competitorList = await getCubingChinaCompetitorList(browser, comp.id);
+//     for (const eventId of compEvents)
+//       for (const roundId of roundIds) {
+//         const link = `https://cubing.com/live/${comp.id
+//           .split(/(?=[A-Z])|(?=\d{4})/)
+//           .join("-")}#!/event/${eventId}/${roundId}/all`;
 
-        // scrape data
-        const resultsData = await getCubingChinaTable(browser, link);
+//         // scrape data
+//         const resultsData = await getCubingChinaTable(browser, link);
 
-        // splice to get rid of the two header row things
-        resultsData.splice(0, 2);
+//         // splice to get rid of the two header row things
+//         resultsData.splice(0, 2);
 
-        // round "out of bounds" if == 0
-        if (resultsData.length == 0) break;
-        for (const r of resultsData) {
-          if (!r) {
-            console.error("0001 Error with ", r);
-            continue;
-          }
-          // process result by making new result object
-          const isMbld = eventId == "333mbf";
-          const ro = new ResultObj();
-          ro.createCubingChinaResult(
-            r[2].text,
-            competitorList[r[1]],
-            eventId,
-            r[3],
-            isMbld ? null : r[4],
-            isMbld ? r[5] : r[6],
-            link
-          );
+//         // round "out of bounds" if == 0
+//         if (resultsData.length == 0) break;
+//         for (const r of resultsData) {
+//           if (!r) {
+//             console.error("0001 Error with ", r);
+//             continue;
+//           }
+//           // process result by making new result object
+//           const isMbld = eventId == "333mbf";
+//           const ro = new ResultObj();
+//           ro.createCubingChinaResult(
+//             r[2].text,
+//             competitorList[r[1]],
+//             eventId,
+//             r[3],
+//             isMbld ? null : r[4],
+//             isMbld ? r[5] : r[6],
+//             link
+//           );
 
-          // check if record or if better than result to beat or if best result so far
-          const {
-            isRecordSingle,
-            isRecordAverage,
-            betterThanBestSoFar,
-            notifySingle,
-            notifyAverage,
-          } = getResultStatus(ro, resultsToBeat, stats);
+//           // check if record or if better than result to beat or if best result so far
+//           const {
+//             isRecordSingle,
+//             isRecordAverage,
+//             betterThanBestSoFar,
+//             notifySingle,
+//             notifyAverage,
+//           } = getResultStatus(ro, resultsToBeat, stats);
 
-          // if so complete the result by adding iso2 and pr tags from WCA API
-          if (
-            isRecordSingle ||
-            isRecordAverage ||
-            betterThanBestSoFar ||
-            notifyAverage ||
-            notifySingle
-          )
-            await ro.addISO2AndTags();
+//           // if so complete the result by adding iso2 and pr tags from WCA API
+//           if (
+//             isRecordSingle ||
+//             isRecordAverage ||
+//             betterThanBestSoFar ||
+//             notifyAverage ||
+//             notifySingle
+//           )
+//             await ro.addISO2AndTags();
 
-          // add to statistics
-          addResultObjToStatistics(
-            stats,
-            ro,
-            betterThanBestSoFar,
-            notifySingle,
-            notifyAverage
-          );
-        }
-      }
-  }
-  console.log("Cubing China results completed");
-}
+//           // add to statistics
+//           addResultObjToStatistics(
+//             stats,
+//             ro,
+//             betterThanBestSoFar,
+//             notifySingle,
+//             notifyAverage
+//           );
+//         }
+//       }
+//   }
+//   console.log("Cubing China results completed");
+// }
 
 async function processWCALiveResults(stats, today, oneWeekAgo, resultsToBeat) {
   // fetch comps between the dates and filter for events
@@ -244,80 +233,80 @@ function initStats() {
  * @returns list of comps from WCA API that are in China and have at least one of the events in eventIds
  * https://wca-rest-api.robiningelbrecht.be/#tag/competition_model
  */
-async function getCubingChinaComps(startDate, endDate) {
-  const url = `https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions/CN.json`;
-  const response = await fetch(url);
-  if (!response.ok) {
-    console.error(`HTTP error! status: ${response.status}`);
-    return;
-  }
-  const compsArray = (await response.json())?.items.filter(
-    (comp) =>
-      new Date(comp.date.from) >= startDate &&
-      new Date(comp.date.till) <= endDate &&
-      comp.events.some((compEventId) => eventIds.has(compEventId))
-  );
-  // compsArray only contains comps in the date range and that have at least one event from eventIds
-  return compsArray;
-}
+// async function getCubingChinaComps(startDate, endDate) {
+//   const url = `https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions/CN.json`;
+//   const response = await fetch(url);
+//   if (!response.ok) {
+//     console.error(`HTTP error! status: ${response.status}`);
+//     return;
+//   }
+//   const compsArray = (await response.json())?.items.filter(
+//     (comp) =>
+//       new Date(comp.date.from) >= startDate &&
+//       new Date(comp.date.till) <= endDate &&
+//       comp.events.some((compEventId) => eventIds.has(compEventId))
+//   );
+//   // compsArray only contains comps in the date range and that have at least one event from eventIds
+//   return compsArray;
+// }
 
 /**
  * Get competitor list
  * @param {string} compId
  * @returns Object with key of the competitor's competition id and value of their WCA ID. The WCA ID will be null if it is a first time competitor
  */
-async function getCubingChinaCompetitorList(browser, compId) {
-  // get competitor list to get wca ids from
-  const competitorTable = await getCubingChinaTable(
-    browser,
-    `https://cubing.com/competition/${compId
-      .split(/(?=[A-Z])|(?=\d{4})/)
-      .join("-")}/competitors`
-  );
-  competitorTable.splice(0, 3);
-  // get competitor list object that has key of competitor id and value of wca id so can check wca api for PRs
-  const competitorList = competitorTable.reduce((accumulator, item) => {
-    accumulator[item[0]] = item[1].hasOwnProperty("link")
-      ? item[1].link.split("/").pop()
-      : null;
-    return accumulator;
-  }, {});
-  return competitorList;
-}
+// async function getCubingChinaCompetitorList(browser, compId) {
+//   // get competitor list to get wca ids from
+//   const competitorTable = await getCubingChinaTable(
+//     browser,
+//     `https://cubing.com/competition/${compId
+//       .split(/(?=[A-Z])|(?=\d{4})/)
+//       .join("-")}/competitors`
+//   );
+//   competitorTable.splice(0, 3);
+//   // get competitor list object that has key of competitor id and value of wca id so can check wca api for PRs
+//   const competitorList = competitorTable.reduce((accumulator, item) => {
+//     accumulator[item[0]] = item[1].hasOwnProperty("link")
+//       ? item[1].link.split("/").pop()
+//       : null;
+//     return accumulator;
+//   }, {});
+//   return competitorList;
+// }
 
-/**
- * Get data from a Cubing.com table
- * @param {puppeteer browser} browser
- * @param {string} link
- * @returns
- */
-async function getCubingChinaTable(browser, link) {
-  const page = await browser.newPage();
+// /**
+//  * Get data from a Cubing.com table
+//  * @param {puppeteer browser} browser
+//  * @param {string} link
+//  * @returns
+//  */
+// async function getCubingChinaTable(browser, link) {
+//   const page = await browser.newPage();
 
-  await page.goto(link);
+//   await page.goto(link);
 
-  // Wait for 3 seconds for weird loading stuff
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+//   // Wait for 3 seconds for weird loading stuff
+//   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  // Now you can safely scrape the table content
-  const tableData = await page.evaluate(() => {
-    // Select the table within the '.table-responsive' class
-    const rows = document.querySelectorAll(".table-responsive table tr");
+//   // Now you can safely scrape the table content
+//   const tableData = await page.evaluate(() => {
+//     // Select the table within the '.table-responsive' class
+//     const rows = document.querySelectorAll(".table-responsive table tr");
 
-    // Extract data from the rows
-    return Array.from(rows, (row) =>
-      Array.from(row.cells, (cell) => {
-        const anchor = cell.querySelector("a"); // Find <a> elements in the cell
-        return anchor
-          ? { text: anchor.innerText, link: anchor.href }
-          : cell.innerText; // Return link and text or cell text
-      })
-    );
-  });
+//     // Extract data from the rows
+//     return Array.from(rows, (row) =>
+//       Array.from(row.cells, (cell) => {
+//         const anchor = cell.querySelector("a"); // Find <a> elements in the cell
+//         return anchor
+//           ? { text: anchor.innerText, link: anchor.href }
+//           : cell.innerText; // Return link and text or cell text
+//       })
+//     );
+//   });
 
-  await page.close(); // Close the page
-  return tableData;
-}
+//   await page.close(); // Close the page
+//   return tableData;
+// }
 
 /**
  * Add result info to statistics
@@ -403,7 +392,7 @@ function addSummaryObjToNotify(summaryObj, stats) {
   } else stats.notify.push(summaryObj);
 }
 
-function makeSummaryEmbed(stats) {
+function makeSummaryEmbeds(stats) {
   const recordTypes = new Set(["NR", "CR", "WR"]);
   stats.notify.sort((a, b) => a.compare(b));
   const lines = [];
@@ -428,11 +417,25 @@ function makeSummaryEmbed(stats) {
     lines.push(line);
   }
 
-  const embed = new EmbedBuilder()
-    .setTitle("Weekly BLD Summary")
-    .setDescription(lines.length > 0 ? lines.join("\n") : "No results / error")
-    .setColor(0x7289dd);
-  return embed;
+  const embeds = lines.reduce((acc, cur) => {
+    const lastLen = acc.at(-1)?.totalLength || null;
+    if(lastLen === null || lastLen+cur.length+1 > 4096){ acc.push({lines: [], totalLength: 0}); }
+    const last = acc.at(-1);
+    if(last){ 
+      last.lines.push(cur); 
+      last.totalLength += cur.length + 1; // +1 for \n later
+    }
+    return acc;
+  }, []).flatMap((item, i) => {
+    if(item.totalLength === 0){return [];} // idk why it would be 0
+    const eb = new EmbedBuilder();
+    eb.setColor(0x7289dd);
+    if(i===0){ eb.setTitle("Weekly BLD Summary"); }
+    eb.setDescription(item.lines.length > 0 ? item.lines.join("\n") : "No results");
+    return [eb];
+  });
+
+  return embeds;
 }
 
 function makeStatsEmbed(stats) {
